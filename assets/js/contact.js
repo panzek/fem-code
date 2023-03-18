f2be619a56bbfdcfaf391e5ff1a2d100fc27ebf4
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-03-18 15:22:17 +0000

    Add function to capture submit event and submit form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,24 @@
-// Get elements from DOM and store their values 
+
 
 document.addEventListener('DOMContentLoaded', function() {
     // add code
+    let contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', handleSubmit);
+
+    function handleSubmit(event) {
+        // prevent form submission 
+        event.preventDefault();
+
+        // Get elements from the DOM and store their values 
+        let name = document.getElementById('name').value;
+        console.log(name)
+        let email = document.getElementById('email').value;
+        console.log(email)
+        let subject = document.getElementById('subject').value;
+        console.log(subject)
+        let message = document.getElementById('message').value;
+        console.log(message)     
+
+    }
+
 })
